2ca0f3bc69f47c8eba855cb92b40a4b40d3f4e24
Alistair Smith <alistairsmith01@gmail.com>
2021-08-03 00:53:42 +0100

    feat: Add tailwidn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import {AppProps} from 'next/app';
+import 'tailwindcss/tailwind.css';
 
 export default function App({Component, pageProps}: AppProps) {
 	return <Component {...pageProps} />;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	purge: ['src/**/*.{ts,tsx}'],
+	darkMode: false,
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+	mode: 'jit',
+};
