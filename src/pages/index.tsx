1ffa8cb78762a570115120fabc1d9dbe21359f37
Alistair Smith <alistairsmith01@gmail.com>
2021-08-03 01:07:36 +0100

    Nice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,45 @@
+import {motion, Variants} from 'framer-motion';
+
+const sentence: Variants = {
+	hidden: {
+		opacity: 1,
+	},
+	visible: {
+		opacity: 1,
+		transition: {
+			delay: 0.5,
+			staggerChildren: 0.08,
+		},
+	},
+};
+
+const letter: Variants = {
+	hidden: {opacity: 0, y: 50},
+	visible: {opacity: 1, y: 0},
+};
+
+const mapper = (text: string) =>
+	text.split('').map((char, index) => (
+		<motion.span key={`${char}-${index.toString()}`} variants={letter}>
+			{char}
+		</motion.span>
+	));
+
 export default function Home() {
-	return <div>YOOO</div>;
+	return (
+		<div className="h-screen bg-black text-white flex justify-center items-center">
+			<div>
+				<motion.h3
+					className="text-center font-serif text-5xl"
+					variants={sentence}
+					initial="hidden"
+					animate="visible"
+				>
+					{mapper('Kevin')}
+					<br />
+					{mapper('Thomas')}
+				</motion.h3>
+			</div>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const {fontFamily} = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	purge: ['src/**/*.{ts,tsx}'],
 	darkMode: false,
 	theme: {
+		fontFamily: {
+			...fontFamily,
+			serif: ['Cormorant Garamond', 'sans-serif'],
+			sans: ['Open Sans', 'sans-serif'],
+		},
 		extend: {},
 	},
 	variants: {
