0c1e09882d2d72e742de36d5fc4cb8cef2a4a1d9
Alistair Smith <alistairsmith01@gmail.com>
2021-08-04 15:36:28 +0100

    smooth asff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import {motion} from 'framer-motion';
 import {Down} from '../components/icons';
 
+function getY() {
+	const mult = 100;
+	const rand = Math.random();
+
+	if (rand > 0.5) {
+		return rand * mult;
+	}
+
+	return -rand * mult;
+}
+
 const mapper = (text: string) =>
 	text.split('').map((char, index) => (
 		<motion.span
 			key={`${char}-${index.toString()}`}
 			className="inline-block"
-			variants={{hidden: {opacity: 0, y: 20}, visible: {opacity: 1, y: 0}}}
+			variants={{
+				hidden: {opacity: 0, y: getY()},
+				visible: {opacity: 1, y: 0},
+			}}
 		>
 			{char}
 		</motion.span>
