29a88003e9864275b861b5a8b54193bbb6064c0d
Alistair Smith <alistairsmith01@gmail.com>
2021-09-08 01:24:08 +0100

    win 98 rewrite

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,5 +4,9 @@ import 'tailwindcss/tailwind.css';
 import '../styles/index.css';
 
 export default function App({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<div className="absolute top-0 left-0 right-0 bottom-0">
+			<Component {...pageProps} />
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
 	purge: ['src/**/*.{ts,tsx}'],
 	darkMode: false,
 	theme: {
-		fontFamily: {
-			...defaultTheme.fontFamily,
-			mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
-			sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
-		},
 		extend: {},
 	},
 	variants: {
