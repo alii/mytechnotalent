d07e74aabc13dced692db0cea827438a4037487e
Alistair Smith <alistairsmith01@gmail.com>
2021-09-08 13:30:37 +0100

    h

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,17 @@ export default function Index() {
 
 	return (
 		<div className="flicker bg-teal overflow-hidden rounded-md h-full flex flex-col">
-			<div className="flex-1 p-3 text-shadow">
-				<h1>Kevin Thomas</h1>
+			<div className="flex-1 p-4 text-right text-shadow space-y-3">
+				<div>
+					<h1 className="text-2xl">Kevin Thomas</h1>
+					<p>Senior Software Engineer in Test</p>
+				</div>
+				<div className="bg-black inline-block bg-opacity-10 rounded-sm p-0.5 px-1.5">
+					<p>user: kevin</p>
+					<p>cpu: 43℃</p>
+				</div>
 			</div>
-			<div className="bg-timberwolf p-1 flex space-x-1">
+			<div className="bg-timberwolf p-0.5 flex space-x-1">
 				<button
 					type="button"
 					className="flex space-x-1 items-center text-sm border-2 border-timberwolf-800 px-2"
